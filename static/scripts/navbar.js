66222b403be71c631efa85ed845ce5d6a17bb45b
Nikoscocos <77122703+Nikoscocos@users.noreply.github.com>
2022-02-24 23:07:14 +0400

    Add files via upload

---
+++ static/scripts/navbar.js
@@ -0,0 +1,12 @@
+function showNav() {
+    document.getElementById('navbar').style.left = '0px'
+    document.getElementById('back').style.opacity = '1'
+    document.getElementById('back').style.left = '0'
+}
+function remvNav() {
+    document.getElementById('navbar').style.left = '-260px'
+    document.getElementById('back').style.opacity = '0'
+}
+function changeTitle(title) {
+    document.getElementById('navtitl').innerHTML = title
+}
